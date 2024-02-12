e8d02c0a026701c52009120855b111431fa9155e
donmoreno09 <morenodaqm@gmail.com>
2024-02-12 19:36:03 +0100

    first try

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title> HOME </title>
+</head>
+<body>
+
+    <h3> Login </h3>
+
+        <form action="login" method = "post">
+            <input type="text" id="username" name="username" placeholder="Username"><br>
+            <input type="password" id="pwd" name="pwd" placeholder="Password"><br><br>
+            <input type="submit" value="LOGIN">
+        </form>
+
+    <h3> Signup </h3>
+
+        <form action="signup" method = "post">
+            <input type="text" id="username" name="username" placeholder="Username"><br>
+            <input type="password" id="pwd" name="pwd" placeholder="Password"><br><br>
+            <input type="email" id="email" name="email" placeholder="Email"><br><br>
+            <input type="submit" value="SUBMIT">
+        </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('signup', function () {
+    return view('index');
 });
