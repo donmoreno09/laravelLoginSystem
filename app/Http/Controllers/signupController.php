28a33c0da6025c12b5268bd71a60120f696a0af6
donmoreno09 <morenodaqm@gmail.com>
2024-02-12 20:02:42 +0100

    mi sono fermato sul controllo se il username esiste gia in database perchè ci sono ancora delle lacune

--- app/Http/Controllers/signupController.php
+++ app/Http/Controllers/signupController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class signupController extends Controller
 {
-    
+    public function checkErrors(){
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            $username = $_POST['username'];
+            $password = $_POST['pwd'];
+            $email = $_POST['email'];
+
+            $errors = [];
+
+            if(empty($username) || empty($password) || empty($email)){
+                $errors['empty_input'] = "Fill in all fields!";
+            }
+
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $errors['email_invalid'] = "Email invalid";
+            }
+
+            
+
+
+
+
+        }
+    }
 }
